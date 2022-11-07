23bf16e71992a5c96f04ff6110a6c9be021d5fa3
Bharath <trbharath7@gmail.com>
2022-11-07 18:11:36 +0530

    asset link fix

--- pages/login.js
+++ pages/login.js
@@ -110,7 +110,7 @@ export default function Login() {
       </div>
       <div className={styles.img__container}>
         <img
-          src="/assets/login_img.jpg"
+          src="/login_img.jpg"
           alt="login illustration"
           className={styles.img}
         />
--- pages/signup.js
+++ pages/signup.js
@@ -128,7 +128,7 @@ export default function Signup() {
       </div>
       <div className={styles.img__container}>
         <img
-          src="/assets/login_img.jpg"
+          src="/login_img.jpg"
           alt="login illustration"
           className={styles.img}
         />
